31d980a70a58eeb9194fdd9e5c9d43ccf586054a
tmznwnel00 <tmznwnel00@gmail.com>
2023-12-15 21:20:22 +0900

    feat: apply post api in home page

--- src/views/Mat-Home.vue
+++ src/views/Mat-Home.vue
@@ -4,7 +4,7 @@
     <img class="profile" src="../assets/profile_image.png">
     <p class="paragraph" style="top: 55px; font-weight: 200;">나의 맛BTI를 파악하고</p>
     <p class="paragraph" style="top: 30px; font-weight: 100;">맛집과 메뉴를 추천받아요</p>
-    <Button class="Button_h" v-on:click="push_about">시작하기</Button>
+    <Button class="Button_h" v-on:click="push_about(); use_api()">시작하기</Button>
     <p class="titledocs">오늘 점심 뭐먹지? 어디서 식사하지? 고민하는 시간이 <br> 아까운 당신! 빠르게 정보를 찾아보아요.</p>
     <div @click="modal_isOpen = true">
       <img class="share_small" src="../assets/Share_small.png">
@@ -15,13 +15,22 @@
 
 <script>
 import router from '../router';
+import axios from 'axios';
 
 // import TitleScreen from './components/TitleScreen.vue'
 
 export default {
   methods : {
     push_about(){
+      console.log('hi');
       router.push("/page1");
+    },
+    async use_api(){
+      try {const response = await axios.post('http://110.165.19.54:5000/chat');
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
     }
   }
 }
